dc6462acbebd00801e6f03116da7991bc1bef97e
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-10-22 15:08:03 +0530

    type scipt

---
+++ arr.ts
@@ -0,0 +1,10 @@
+// Create a function sumArray that takes an array of numbers and returns their total sum
+function sum(arr:Array<number>):number{
+ 
+ let sum3:number = arr.reduce((acc:number,ele:number)=>{ return acc+ele},0)
+   return sum3
+}
+
+let arr:number[] = [1,2,3,4,5,6]
+console.log(sum(arr));
+
