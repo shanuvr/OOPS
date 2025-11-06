2fff47cbf22a1ca17ee0dab81f55ffcb9f19b140
Shanu V Ramesan <shanuvr112233@gmail.com>
2025-11-06 08:33:26 +0530

    ts

--- arr.ts
+++ arr.ts
@@ -3,7 +3,7 @@ function sum(arr:Array<number>):number{
  
  let sum3:number = arr.reduce((acc:number,ele:number)=>{ return acc+ele},0)
    return sum3
-}drrdadfd
+}dr
 let arr:number[] = [1,2,3,4,5,6,7,8]
 console.log(sum(arr));
 
