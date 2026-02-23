330b91e063b628614d8cfde6ddaa0afef446f899
Shanu <shanuvr112233@gmail.com>
2026-02-23 20:33:37 +0530

    Update INH.ts

--- INH.ts
+++ INH.ts
@@ -1,10 +1,10 @@
-class User{
+ class User{
     public name:string;
      #age:number;
     constructor(name:string,age:number){
         this.name = name;
         this.#age  = age;
-
+ 
     }
 
     changeAge(newAge:number){
